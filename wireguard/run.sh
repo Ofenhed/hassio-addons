2d1a3f78301d08a3f727106de6bd80b013770a1a
Marcus Ofenhed <marcus.ofenhed@conditionraise.se>
2026-09-13 14:35:20 +0200

    logging

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -22,32 +22,43 @@ block_non_wireguard=$(bashio::config 'block_non_wireguard' false)
 
 function teardown_wg() {
     set +e
+    echo "Removing interface"
     ip link delete "$wg_interface_name"
+    echo "Removing routing rule"
     ip rule del not fwmark "$fwmark" table "$route_table_id"
+    echo "Flushing route table"
     ip route flush table "$route_table_id"
 }
 
 trap teardown_wg EXIT
 
+echo "Creating interface $wg_interface_name"
 ip link "$wg_interface_name" 2>/dev/null || ip link add "$wg_interface_name" type wireguard
 
 conf_file=$(mktemp)
 
 bashio::config 'wg_config' > "$conf_file"
 
+echo "Applying config"
 wg setconf "$wg_interface_name" "$conf_file"
 rm -f "$conf_file"
 
+echo "Adding routing rule"
 ip rule add not fwmark "$fwmark" table "$route_table_id"
+ip rule
 if [ $block_non_wireguard = "true" ]; then
     ip route add table "$route_table_id" to blackhole default priority 100
 fi
 
+echo "Setting private key"
 wg set "$wg_interface_name" private-key <(cat <<<"$private_key")
 unset private_key
+echo "Public key is $(wg get "$wg_interface_name" public-key)"
 
+echo "Bringing $wg_interface_name up"
 ip link set "$wg_interface_name" up
 ip route add table "$route_table_id" to default dev "$wg_interface_name" priority 1
+ip route show table "$route_table_id"
 
 wg show
 if [ "$log_status_interval" -eq 0 ]; then
