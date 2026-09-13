f8f9ca68b70d9059726b9c7b4bcabb94edf16c53
Marcus Ofenhed <marcus.ofenhed@conditionraise.se>
2026-09-13 16:07:22 +0200

    Why not..?

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -59,6 +59,7 @@ unset private_key
 echo "Bringing $wg_interface_name up"
 ip link set "$wg_interface_name" up
 
+ecoh "Creating routing table $route_table_id"
 for peer_ip in $(wg show "$wg_interface_name" allowed-ips); do
     ip route add table "$route_table_id" to "$peer_ip" dev "$wg_interface_name" priority 1
 done
