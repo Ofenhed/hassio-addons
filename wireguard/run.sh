d0985f8432c78181d3ef34d9f49001b31a7e8ff5
Marcus Ofenhed <marcus.ofenhed@conditionraise.se>
2026-09-13 16:08:54 +0200

    fix allowed ips

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -60,7 +60,7 @@ echo "Bringing $wg_interface_name up"
 ip link set "$wg_interface_name" up
 
 ecoh "Creating routing table $route_table_id"
-for peer_ip in $(wg show "$wg_interface_name" allowed-ips); do
+for peer_ip in $(wg show "$wg_interface_name" allowed-ips | cut -f 2-); do
     ip route add table "$route_table_id" to "$peer_ip" dev "$wg_interface_name" priority 1
 done
 
